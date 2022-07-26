c2aabf455f43940db31f95c5af0e54415154e59d
Andy Elliott <15898957+andye2004@users.noreply.github.com>
2022-07-26 10:29:24 +0800

    alternative

--- src/main/java/github/andye/intellijsbbeansissue/IntellijSbBeansIssueApplication.java
+++ src/main/java/github/andye/intellijsbbeansissue/IntellijSbBeansIssueApplication.java
@@ -1,9 +1,11 @@
 package github.andye.intellijsbbeansissue;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class IntellijSbBeansIssueApplication {
 
     public static void main(String[] args) {
